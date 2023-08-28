c361dd21c2ee1630b447f3ca910b85eabff74b1c
Ozurumba Onwuasoanya <onwuasoanyaozurumba007@gmail.com>
2023-08-28 09:03:37 +0100

    add a http package to the pubspec.yaml file \ added a method of getTeams in the home page

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,7 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
+
+  // get teams
+  Future getTeams() async {
+    var response = await http.get(
+      Uri.http('balldontlie.io', 'api/v1/teams'),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
