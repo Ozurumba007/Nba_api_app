7062ca064fffa74cfcad4cd0a6165ddd0f11fc24
Ozurumba Onwuasoanya <onwuasoanyaozurumba007@gmail.com>
2023-08-29 18:32:39 +0100

    added a model to the team.dart file

---
+++ lib/model/team.dart
@@ -0,0 +1,10 @@
+class Team {
+  final String fullName;
+  final String abbreviation;
+  final String city;
+  Team({
+    required this.fullName,
+    required this.abbreviation,
+    required this.city,
+  });
+}
